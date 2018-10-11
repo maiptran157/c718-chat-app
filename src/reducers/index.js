842fb4c87995509a3a99405ac4d4c388c9e82edb
Mia Tran <maiptran157@gmail.com>
2018-10-11 16:11:52 -0700

    Complete project-setup

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import chatReducer from './chat_reducer';
+
+const rootReducer = combineReducers({
+    chat: chatReducer
+});
+
+export default rootReducer;
+
